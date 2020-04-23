414be0f18f324ceb3a4d3a99b406925dd4b2a885
Ricardo Alves <ricardo_alves1@hotmail.com>
2020-04-23 15:18:03 -0300

    meu primeiro commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+
+
+function compilaSass() {
+    return gulp
+    .src('css/scss/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+        browsers:['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
+}
+
+gulp.task('sass', compilaSass);
+
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+}
+
+gulp.task('browser-sync', browser);
+
+function watch() {
+    gulp.watch('css/scss/*.scss', compilaSass);
+    gulp.watch(['*.html']).on('change', browserSync.reload);
+}
+
+gulp.task('watch', watch);
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
